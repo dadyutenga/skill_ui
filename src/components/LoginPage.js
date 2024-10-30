2efeb8bd84ced5bef1ae944d3bf2246c62c95d48
Your Name <you@example.com>
2024-10-30 17:07:26 +0300

    login page

---
+++ src/components/LoginPage.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import '../styles/LoginPage.css';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignIn: true,
+    };
+  }
+
+  toggleSignInSignUp = () => {
+    this.setState((prevState) => ({
+      isSignIn: !prevState.isSignIn,
+    }));
+  };
+
+  render() {
+    const { isSignIn } = this.state;
+
+    return (
+      <div className={`container ${isSignIn ? '' : 'sign-up-mode'}`}>
+        <div className="forms-container">
+          <div className="signin-signup">
+            {/* Sign In Form */}
+            <form className="sign-in-form">
+              <h2>Sign in to Skills Realistic</h2>
+              <div className="social-icons">
+                <button type="button" className="social-btn">
+                  <i className="fab fa-facebook-f"></i>
+                </button>
+                <button type="button" className="social-btn">
+                  <i className="fab fa-google"></i>
+                </button>
+                <button type="button" className="social-btn">
+                  <i className="fab fa-linkedin-in"></i>
+                </button>
+              </div>
+              <p className="divider">or use your email account:</p>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <a href="#" className="forgot-password">Forgot your password?</a>
+              <button type="submit" className="btn">SIGN IN</button>
+            </form>
+
+            {/* Sign Up Form */}
+            <form className="sign-up-form">
+              <h2>Create Account</h2>
+              <div className="social-icons">
+                <button type="button" className="social-btn">
+                  <i className="fab fa-facebook-f"></i>
+                </button>
+                <button type="button" className="social-btn">
+                  <i className="fab fa-google"></i>
+                </button>
+                <button type="button" className="social-btn">
+                  <i className="fab fa-linkedin-in"></i>
+                </button>
+              </div>
+              <p className="divider">or use your email for registration:</p>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Name" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <button type="submit" className="btn">SIGN UP</button>
+            </form>
+          </div>
+        </div>
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here?</h3>
+              <p>Sign up and discover more with us!</p>
+              <button className="btn transparent" onClick={this.toggleSignInSignUp}>SIGN UP</button>
+            </div>
+          </div>
+
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us?</h3>
+              <p>If you already have an account, sign in here.</p>
+              <button className="btn transparent" onClick={this.toggleSignInSignUp}>SIGN IN</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
